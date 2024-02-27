389486b591bcf3cd7068ecc4217b5f70a207152c
gabrielliosc <gabrielli.osc@gmail.com>
2024-02-27 00:44:44 -0300

    feat: Update routes and Create sessions route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 const { Router } = require("express")
 
 const usersRoutes = require('./users.routes')
+const sessionsRoutes = require("./sessions.routes")
 const movieNotesRoutes = require('./movies_notes.routes')
 const movieTagsRoutes = require('./movies_tags.routes')
 
 const routes = Router()
 
 routes.use('/users', usersRoutes)
+routes.use('/sessions', sessionsRoutes)
 routes.use('/movies', movieNotesRoutes)
 routes.use('/tags', movieTagsRoutes)
 
